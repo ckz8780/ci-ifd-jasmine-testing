7796b1e6b6f57321565461a4640f6c5545c06d42
Chris Zielinski <ckz8780@gmail.com>
2018-05-08 23:01:30 +0000

    updated specs to work with both function types

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,20 +1,23 @@
-/*function addition(numOne, numTwo) {
+// Tested with the New Calculator specs in calcSpec.js
+Calculator = function() {
+    this.value = 0;
+};
+
+Calculator.prototype.add = function(number) {
+    if(typeof(number) == 'number'){
+        this.value += 'number';
+        console.log(this.value);
+    } else {
+        alert('Argument must be a number');
+    }
+};
+
+//Tested with the Calculator specs in calcSpec.js
+function addition(numOne, numTwo) {
     if(typeof(numOne) == 'number' && typeof(numTwo) == 'number'){
         return numOne + numTwo;
     } else {
         alert('Both arguments must be numbers');
     }
-}*/
-
-Calculator = function() {
-    this.value = 0;
-};
-
-Calculator.prototype.add = function(number)  {
-    if(typeof(number) == 'number'){
-        this.value += "number";
-    } else {
-        alert('Argument must be a number');
-    }
-    
 }
+
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,3 +1,4 @@
+// Calculator Specs:
 describe("Calculator", function() {
     describe("Addition function", function() {
         it("should take two arguments and return the sum.", function() {
@@ -12,6 +13,34 @@ describe("Calculator", function() {
             spyOn(window, "alert");
             addition("hello");
             expect(window.alert).toHaveBeenCalledWith("Both arguments must be numbers")
+        });
+    });
+});
+
+// New Calculator Specs:
+describe("New Calculator", function() {
+    
+    beforeEach(function() {
+        calc = new Calculator();
+    });
+    
+    describe("Add method", function() {
+        it("should take two arguments and return the sum.", function() {
+            calc.add(2);
+            calc.add(2);
+            expect(calc.value).toBe(4);
+        });
+        
+        it("should not return 4 if the arguments given don't produce a sum of 4.", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
+        });
+        
+        it("should have called the alert function if either number is undefined.", function() {
+            spyOn(window, "alert");
+            calc.add("Hello");
+            expect(window.alert).toHaveBeenCalledWith("Argument must be a number");
         });
     });
 });

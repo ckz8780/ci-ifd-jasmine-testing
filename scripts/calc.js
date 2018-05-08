1fa50e6b240517784fc3ba01cc59a10a3eff672e
Chris Zielinski <ckz8780@gmail.com>
2018-05-08 22:22:48 +0000

    broke all code by commenting out original calc function

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,20 @@
-function addition(numOne, numTwo) {
+/*function addition(numOne, numTwo) {
     if(typeof(numOne) == 'number' && typeof(numTwo) == 'number'){
         return numOne + numTwo;
     } else {
         alert('Both arguments must be numbers');
     }
+}*/
+
+Calculator = function() {
+    this.value = 0;
+};
+
+Calculator.prototype.add = function(number)  {
+    if(typeof(number) == 'number'){
+        this.value += "number";
+    } else {
+        alert('Argument must be a number');
+    }
+    
 }
